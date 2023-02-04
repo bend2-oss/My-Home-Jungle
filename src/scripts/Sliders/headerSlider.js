031f46e3f8f7bddaeb8f2fbf1262c1c3e744d612
RinatNuriev <justinnuriev@gmail.com>
2023-02-04 21:15:49 +0300

    Delete

--- src/scripts/Sliders/headerSlider.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    new ItcSimpleSlider('.itcss', {
-        loop: true,
-        autoplay: true,
-        interval: 5000,
-        swipe: true,
-    });
-});
-
