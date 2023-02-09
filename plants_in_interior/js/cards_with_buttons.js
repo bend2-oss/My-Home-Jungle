6546d6504694cb2d48113a01384062f3a419e757
polina4k <polina444k@gmail.com>
2023-02-09 17:54:06 +0300

    commit

--- plants_in_interior/js/cards_with_buttons.js
+++ plants_in_interior/js/cards_with_buttons.js
@@ -1,17 +1,84 @@
-$('.card').hover(function(){
+let currentElem = null
+
+$(document).on('mouseover', '.card', function(event){
+
+    if(currentElem) return
+
+    let target = event.target.closest('.card')
+
+    if(!target) return
+
+    currentElem = target
+
+    let buttons = $(this).children('.buttons-container')
     $(this).children('.card-image').css({
         'filter':'brightness(40%)',
         'transition':'1s'
     })
-    $(this).children('.card-button').fadeIn()
-    $(this).children('.card-button-like').fadeIn()
-    // console.log('there');
-}, function(){
+    buttons.children('.card-button').fadeIn()
+    buttons.children('.card-button-like').fadeIn()
+})
+
+$(document).on('mouseout', '.card', function(event){
+
+    if(!currentElem) return
+
+    let relatedTarget = event.relatedTarget
+
+    while(relatedTarget){
+        if(relatedTarget == currentElem) return;
+
+        relatedTarget = relatedTarget.parentNode;
+    }
+
+    currentElem = null
+    let buttons = $(this).children('.buttons-container')
+    
     $(this).children('.card-image').css({
         'filter':'brightness(100%)',
         'transition':'1s'
     })
-    $(this).children('.card-button').fadeOut()
-    $(this).children('.card-button-like').fadeOut()
-    
+    buttons.children('.card-button').fadeOut()
+    buttons.children('.card-button-like').fadeOut()
+
 })
+
+// $('.card').hover(function(){
+// $(this).children('.card-image').css({
+//     'filter':'brightness(40%)',
+//     'transition':'1s'
+// })
+// $(this).children('.card-button').fadeIn()
+// $(this).children('.card-button-like').fadeIn()
+// // console.log('there');
+// }, function(){
+// $(this).children('.card-image').css({
+//     'filter':'brightness(100%)',
+//     'transition':'1s'
+// })
+// $(this).children('.card-button').fadeOut()
+// $(this).children('.card-button-like').fadeOut()
+
+// })
+
+
+// $(document).on('mouseover', '.card', function(){
+//     $(this).children('.card-image').css({
+//         'filter':'brightness(40%)',
+//         'transition':'1s'
+//     })
+//     $(this).children('.card-button').fadeIn()
+//     $(this).children('.card-button-like').fadeIn()
+//     console.log('there');
+// })
+
+// $(document).on('mouseout', '.card', function(){
+//     $(this).children('.card-image').css({
+//         'filter':'brightness(100%)',
+//         'transition':'1s'
+//     })
+//     $(this).children('.card-button').fadeOut()
+//     $(this).children('.card-button-like').fadeOut()
+// })
+
+
