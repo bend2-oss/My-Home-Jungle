13abea7b7dd86cbbba39bbb97d3fb6953b09dc7a
polina4k <polina444k@gmail.com>
2023-02-21 18:20:59 +0300

    Страница Журнал с выборкой

--- magazine/backend/login.php
+++ magazine/backend/login.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = 'localhost';
 $db_name = 'my-home-jungle_db';
-$db_username = 'rinat';
-$db_password = 'sweethome89';
+$db_user = 'rinat';
+$db_pass = 'sweethome89';
 ?>
--- magazine/js/selection.js
+++ magazine/js/selection.js
@@ -2,17 +2,59 @@
 //     $(this).parent().prepend($('<li class="select-item">Все</li>'))
 // }
 
-$('.select-item').on('click', function () {
-    // if($('.select-head').text() !== 'Все'){
-    //     $(this).parent().prepend($('<li class="select-item">Все</li>'))
-    // }
+$(document).ready(function(){
+    let type = ''
 
-    $(this).parent().prev().text($(this).text());
-    $(this).parent().prev().prev().val($(this).text());
-    // $(this).parent().css({
-    //     '-webkit-transform': 'scaleY(0)',
-    //     '-ms-transform': 'scaleY(0)',
-    //     'transform': 'scaleY(0)'
-    // });
-    console.log(($(this).parent().prev().prev().val()));
-});
+
+    $('.select-item').on('click', function () {
+        // if($('.select-head').text() !== 'Все'){
+        //     $(this).parent().prepend($('<li class="select-item">Все</li>'))
+        // }
+
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+        // $(this).parent().css({
+        //     '-webkit-transform': 'scaleY(0)',
+        //     '-ms-transform': 'scaleY(0)',
+        //     'transform': 'scaleY(0)'
+        // });
+        type = $(this).parent().prev().prev().val();
+        selectionArticles(type)
+    });
+
+    
+    function selectionArticles(type){
+        console.log('вывод из функции' + type)
+
+        $.ajax({
+            url:     "backend/selectionArticles.php", //url страницы
+            type:     "GET", //метод отправки
+            dataType: "json", //формат данных
+            data: {"type": type},
+            success: function(response) { //Данные отправлены успешно
+                if(response.code != 'error'){
+
+                    let i1 = parseInt( response.info.length / 3, 10)
+                    let i2 = parseInt( response.info.length / 3 * 2, 10)
+
+                    for(i = 0; i < response.info.length; i++){
+                        row = response.info[i];
+                        localStorage.setItem('id_company', index);
+                        $('#name').html(row[1]);
+                        $('#inn').html(row[2]);
+                        $('#info').html(row[3]);
+                        $('#dir').html(row[4]);
+                        $('#address').html(row[5]);
+                        $('#tel').html(row[6]);
+                    }
+                }else{
+                    console.log(response);
+                }
+          },
+          error: function(response) { // Данные не отправлены
+                console.log(response);
+          }
+      });
+    }
+})
+
